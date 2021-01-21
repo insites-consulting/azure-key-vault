0c3b78a503f21d2b3510d610741b86e9608360b1
Stephen Smith <stephen.smith@jointhedotsmr.com>
2021-01-21 10:34:55 +0000

    Use better name for vault-name env var

--- config/vault.php
+++ config/vault.php
@@ -4,5 +4,5 @@ return [
     'tenant_id' => env('AZURE_AD_TENANT_ID'),
     'client_id' => env('AZURE_AD_CLIENT_ID'),
     'client_secret' => env('AZURE_AD_CLIENT_SECRET'),
-    'vault' => env('AZURE_KEY_VAULT_ID'),
+    'vault' => env('AZURE_KEY_VAULT_NAME'),
 ];
